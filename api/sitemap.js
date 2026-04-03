6673beb62fb64c7ac0339a9cac3fd63395329ab3
Md. Al Amin <alaminsarkar863@gmail.com>
2026-04-03 12:04:36 +0600

    fixing dynamic  sitemap with vercel serverless function

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,5 +1,5 @@
-import connectDB from '../lib/db.js';
-import Course from '../models/Course.js';
+import connectDB from './lib/db.js';
+import Course from './models/Course.js';
 
 export default async function handler(req, res) {
   try {
